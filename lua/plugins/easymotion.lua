8225c0c8f3bcd6edc40ab038c0cbc1601773b751
Yotsuba <happy819tw@gmail.com>
2024-10-31 20:13:38 +0800

    把 folke/noice.nvim 又裝回來了
    我直接到 ~/.local/share/nvim/lazy/vim-easymotion/autoload/EasyMotion.vim 這個檔案
    
    找到 call s:Message('Jumping to [' . coords[0] . ', ' . coords[1] . ']') 直接註解掉
    
    讓 easymotion 最後不要提示訊息就好

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -1,3 +1,14 @@
+-- 以下的設定跟 folke/noice.nvim 有衝突
+--
+-- 我設定 / 會用 easymotion 開始搜尋，然後可以按字母跳轉到任意位置
+--
+-- 但是看起來 noice.nvim 的提示視窗會讓 easymotion 多跳一行
+--
+--
+-- 建議直接到 ~/.local/share/nvim/lazy/vim-easymotion/autoload/EasyMotion.vim 修改原始碼後再使用
+--
+-- 找到 call s:Message('Jumping to [' . coords[0] . ', ' . coords[1] . ']') 直接註解掉
+--
 return {
   "easymotion/vim-easymotion",
   config = function()
