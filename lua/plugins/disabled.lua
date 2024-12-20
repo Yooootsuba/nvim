581a5a98ccc7e5fb93c032488ade06cb14a50312
Yotsuba <happy819tw@gmail.com>
2024-12-20 22:11:55 +0800

    nvim-lua/plenary.nvim 會害我按 Ctrl + S 的時候游標多往下一行
    但關掉這個插件又會有其他 BUG，目前還不知道怎麼辦

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -6,4 +6,8 @@ return {
     -- 但是 s 在 VIM 預設是刪除字元然後進入 INSERT MODE，我不喜歡所以關閉這個插件
     --
     { "folke/flash.nvim", enabled = false },
+
+    -- nvim-lua/plenary.nvim 會害我按 Ctrl + S 的時候游標多往下一行
+    --
+    -- { "plenary.nvim", enabled = false },
 }
